214b99b5deb6c9428e71200b565cabaac9783647
ckybonist <ckybonist@gmail.com>
2021-08-21 20:31:15 +0800

    Add icon for line diagnostic

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -76,3 +76,13 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = {
+      spacing = 4,
+      prefix = ''
+    }
+  }
+)
+
